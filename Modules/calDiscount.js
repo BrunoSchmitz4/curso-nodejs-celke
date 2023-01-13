8eeb1e1bb1d8f8b1ff06326e7023dc8a00165cc0
Bruno.S.Silva <brunosch250@gmail.com>
2023-01-13 17:29:24 -0300

    Adding handlebars

--- Modules/calDiscount.js
+++
@@ -1,7 +0,0 @@
-// var discount = function(valProd, valDisc){
-//     return valProd - valDisc;
-// } //trabalhar com módulos é preciso de funções
-
-// //para usaar esté módule ´preciso usar a palavra reservada do nide
-
-// module.exports = discount;
--- app.js
+++ app.js
@@ -1,42 +1,17 @@
-lear// Fazendo a onexão com o banco de dados usando o sequelize
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
 
-const Sequelize = require('sequelize');
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
-const sequelize = new Sequelize('bruno', 'brunotwo', '123456', {
-    host: 'localhost',
-    dialect: 'mysql'
+//Rotas
+app.get('/pagamento', function(req, res){
+    res.render('pagamento');
 });
 
-sequelize.authenticate().then(function(){
-    console.log('Conexao realizada com sucesso');
-}).catch(function(err){
-    console.log('Erro o realizar a conexao com BD: ' + err)
+app.get('/add-pagamento', function(req, res){
+    res.render('add-pagamento');
 });
 
-
-
-// Passar o conteúdo abaixo para o caderno
-
-// Criando uma tabela no banco de dados (já conectado) usando o sequelize
-
-const Pagamento = sequelize.define('pagamentos', {
-    //Atributos da coluna da tabela
-    nome:{
-        type: Sequelize.STRING, // Tipo de dado
-    },
-    valor: {
-        type: Sequelize.DOUBLE
-        // Para permitir valores nulos basta deixar allowNull = true
-    }
-});
-
-
-// Criar tabel com Sequelize
-// Pagamento.sync({force: true});
-
-
-// Inserir registro no banco de dados
-Pagamento.create({
-    nome: "Energia",
-    valor: 220
-})
+app.listen(8080);
